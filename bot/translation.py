c485d5df37fa532e50ab193d95b07763cd5db371
Levinomad_TG <84772173+levi749@users.noreply.github.com>
2021-06-24 20:16:29 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -5,27 +5,30 @@
 class Translation(object):
     
     START_TEXT = """<b>Hey {}!!</b>
-<i>Am Just A Advance Auto Filter Bot....😉
 
-Just Add Me To Your Group And Channel And Connect Them And See My Pevers 🔥🔥😝
 
-For More Details Click Help Button Below..
-@CrazyBotsz
+<b> ഹായ് 
+ ഞാൻ MOVIE FACTORY ഗ്രൂപ്പിൽ വർക്ക് ചെയ്യുന്ന ഒരു പാവം ഓട്ടോഫിൽട്ടർ ബോട്ടാണ്. എന്ന് കരുതി എന്നെ മറ്റ് ഗ്രൂപ്പിൽ ആഡ് ആക്കാൻ പറ്റില്ല.😜
+ </b>
 </i>"""    
-    
+  
+@MF_CHATGROUP
+
     HELP_TEXT = """
 
+ <b>  ഇവിടെ HELP ഒന്നുമില്ല നീ ഇത്രയും വലുതായി ഇല്ലേ പിന്നെ എന്തിനാ നിനക്ക് HELP 😜 </b>
+  </I>
+@MF_CHATGROUP
             
-@CrazyBotsz
 """
     
     ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
     
-<b>➥ Creator</b> : <b><i><a href="https://t.me/AlbertEinstein_TG">AlbertEinstein_TG</a></i></b>
+<b>➥ Creator</b> : <b><i><a href="https://t.me/JohnDalton_TG">JOHNDALTON_TG</a></i></b>
 
 <b>➥ Language</b> : <code>Python3</code>
 
 <b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
 
-<b>➥ Source Code</b> : <i><a href="https://github.com/AlbertEinsteinTG/Adv-Auto-Filter-Bot">Click Me</a></i>
+<b>➥ Source Code</b> : <i><a href="">Click Me</a></i>
 """
